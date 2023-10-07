4a1d2d32d6b083a371f1fc74b469c4f7a402ed53
Orlean Sousa <orleansilva60@hotmail.com>
2023-10-07 11:16:12 -0300

    estilizando card e iniciando a listView

--- app/src/main/java/com/example/electricarapps/presentation/MainActivity.kt
+++ app/src/main/java/com/example/electricarapps/presentation/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.electricarapps.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
 import com.example.electricarapps.R
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnCalcular: Button
+    lateinit var lista: ListView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,20 +21,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setupView()
         setupListeners()
+        setupList()
     }
 
     fun setupView() {
-//        preco = findViewById(R.id.et_preco_khw)
-//        KmPercorrido = findViewById(R.id.et_km_percorrido)
-//        btnCalcular = findViewById(R.id.btn_calcular)
-//        resultado = findViewById(R.id.tv_resultado)
         btnCalcular = findViewById(R.id.btn_calcular)
+        lista = findViewById(R.id.lv_informacoes)
+    }
+
+    fun setupList(){
+        var dados = arrayOf(
+            "CupCake", "Donut", "Froyo",
+            "GingerBread", "Honeycomb",
+            "Ice Cream Sandwich", "Jelly Bean"
+        )
+        val adapter =  ArrayAdapter(this, android.R.layout.simple_list_item_1, dados)
+        lista.adapter = adapter
     }
 
     fun setupListeners() {
         btnCalcular.setOnClickListener {
-            startActivity(Intent(this,CalcularAutonomiaActivity::class.java))
+            startActivity(Intent(this, CalcularAutonomiaActivity::class.java))
         }
+
     }
 
 
