49e9cf779f4d6a91a81325ec10009600eeda2e13
Orlean Sousa <orleansilva60@hotmail.com>
2023-10-07 17:28:27 -0300

    crianção das listas com ADPTER para melhor usabilidade do app

--- app/src/main/java/com/example/electricarapps/presentation/MainActivity.kt
+++ app/src/main/java/com/example/electricarapps/presentation/MainActivity.kt
@@ -8,12 +8,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.electricarapps.R
+import com.example.electricarapps.presentation.adapter.CarAdapter
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnCalcular: Button
-    lateinit var lista: ListView
+    lateinit var listaCarros: RecyclerView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,17 +29,17 @@ class MainActivity : AppCompatActivity() {
 
     fun setupView() {
         btnCalcular = findViewById(R.id.btn_calcular)
-        lista = findViewById(R.id.lv_informacoes)
+        listaCarros = findViewById(R.id.rv_lista_carros)
     }
 
-    fun setupList(){
+    fun setupList() {
         var dados = arrayOf(
             "CupCake", "Donut", "Froyo",
             "GingerBread", "Honeycomb",
             "Ice Cream Sandwich", "Jelly Bean"
         )
-        val adapter =  ArrayAdapter(this, android.R.layout.simple_list_item_1, dados)
-        lista.adapter = adapter
+        val adapter = CarAdapter(dados)
+        listaCarros.adapter = adapter
     }
 
     fun setupListeners() {
